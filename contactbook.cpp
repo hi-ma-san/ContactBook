00ca2cce14b8e2236a85b81b5cbfa5436d03c2b5
hi-ma-san <41143161@nfu.edu.tw>
2025-11-17 16:36:17 +0800

    Create Project

---
+++ contactbook.cpp
@@ -0,0 +1,90 @@
+#include "contactbook.h"
+#include "ui_contactbook.h"
+
+#include <QDebug>
+#include <QFile>
+#include <QFileDialog>
+
+
+QString mFileName = "./output.txt";
+
+void Write(QString fileName, QString str)
+{
+    QFile mFile(fileName); //mfile檔案 (起始檔案名稱)
+    if (!mFile.open(QFile::WriteOnly | QFile::Text)) //只寫 並且是文字檔
+    {
+        qDebug()<<"File not open!";
+        return;
+    }
+
+    QTextStream out(&mFile);   //out: 文字串流物件 --> mFile:檔案物件
+    //out 只處理文字編碼 用指標把格式化文字寫入檔案物件
+    out<<str; //文字輸出
+    mFile.flush(); //清空緩衝區存到檔案中
+    mFile.close(); //關閉檔案
+}
+
+ContactBook::ContactBook(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::ContactBook)
+{
+    ui->setupUi(this); //透過有人機介面的ContactBook(*ui)來安裝自己(this)的人機介面
+    //開始有人機介面
+    QStringList ColTotle;
+    ui -> tableW->setColumnCount(4); //設定TableWidget 有多少個欄位
+    ColTotle<<tr("學號")<<tr("班級")<<tr("姓名")<<tr("電話");
+    ui -> tableW -> setHorizontalHeaderLabels(ColTotle); //設定欄位
+
+
+}
+
+ContactBook::~ContactBook()
+{
+    delete ui;
+}
+
+void ContactBook::on_insertButton_clicked()
+{
+    QTableWidgetItem *inputCol1, *inputCol2, *inputCol3, *inputCol4;
+
+    inputCol1 = new QTableWidgetItem(QString(ui->lineEdit->text()));
+    inputCol2 = new QTableWidgetItem(QString(ui->lineEdit_2->text()));
+    inputCol3 = new QTableWidgetItem(QString(ui->lineEdit_3->text()));
+    inputCol4 = new QTableWidgetItem(QString(ui->lineEdit_4->text()));
+
+    //添加在哪一個新列 row_index 從0開始
+    int cur_row = ui-> tableW -> rowCount();
+    ui -> tableW -> insertRow(cur_row);
+
+    qDebug() << cur_row << "\n";
+    ui -> tableW -> setItem(cur_row, 0, inputCol1);
+    ui -> tableW -> setItem(cur_row, 1, inputCol2);
+    ui -> tableW -> setItem(cur_row, 2, inputCol3);
+    ui -> tableW -> setItem(cur_row, 3, inputCol4);
+}
+
+void ContactBook::on_exportButton_clicked()
+{
+    QString saveFile = "";
+    int rowCnt = ui -> tableW -> rowCount();
+    int colCnt = ui -> tableW -> columnCount();
+    //檔案對話視窗
+    mFileName = QFileDialog::getSaveFileName(this, "匯出存檔", ".");
+    for (int i = 0; i < rowCnt; i++)
+    {
+        for (int j = 0; j < colCnt; j++)
+        {
+            saveFile += ui -> tableW -> item(i, j) -> text();
+            if (j != colCnt - 1) saveFile += ",";
+        }
+        saveFile += "\n";
+    }
+    Write(mFileName, saveFile);
+}
+
+
+void ContactBook::on_exitButton_clicked()
+{
+    //加入存檔功能
+    close();
+}
---
+++ contactbook.h
@@ -0,0 +1,32 @@
+#ifndef CONTACTBOOK_H
+#define CONTACTBOOK_H
+
+#include <QDialog>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class ContactBook; //有人機介面的ContactBook
+}
+QT_END_NAMESPACE
+
+class ContactBook : public QDialog //沒有人機介面的ContactBook
+{
+    Q_OBJECT
+
+public:
+    ContactBook(QWidget *parent = nullptr);
+    ~ContactBook();
+
+private slots:
+
+    void on_insertButton_clicked();
+
+    void on_exportButton_clicked();
+
+    void on_exitButton_clicked();
+
+private:
+    Ui::ContactBook *ui; //用指標指向有人機介面的ContactBook
+};
+#endif // CONTACTBOOK_H
